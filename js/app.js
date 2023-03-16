896404e61efc08ba8cb7b863e0d749eff88346df
j-frilot <frilotdr@gmail.com>
2023-03-16 12:07:04 -0500

    Added ability to use while offline, error handling, the birthday recognition, and remove excess fluff

--- js/app.js
+++ js/app.js
@@ -1,92 +1,92 @@
 // DOM Elements
 const time = document.getElementById('time'),
-    greeting = document.getElementById('greeting'),
-    name = document.getElementById('name'),
-    focus = document.getElementById('focus');
-
+	greeting = document.getElementById('greeting'),
+	name = document.getElementById('name'),
+	focus = document.getElementById('focus');
+//Date Objects
+let today = new Date(),
+	hour = today.getHours();
+const day = today.getUTCDate();
+const month = today.getUTCMonth();
 // Options
 const showAmPm = true;
 
 // Show Time
-function showTime() {
-    let today = new Date(),
-        hour = today.getHours(),
-        min = today.getMinutes(),
-        sec = today.getSeconds();
+const showTime = () => {
+	let today = new Date(),
+		hour = today.getHours(),
+		min = today.getMinutes(),
+		sec = today.getSeconds();
 
-    // Set AM or PM
-    const amPm = hour >= 12 ? 'PM' : 'AM';
+	// Set AM or PM
+	const amPm = hour >= 12 ? 'PM' : 'AM';
 
-    // 12hr Format
-    hour = hour % 12 || 12;
+	// 12hr Format
+	hour = hour % 12 || 12;
 
-    // Output Time
-    time.innerHTML = `${hour}<span>:</span>${addZero(
-        min
-    )}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
+	// Output Time
+	time.innerHTML = `${hour}<span>:</span>${addZero(
+		min
+	)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
-    setTimeout(showTime, 1000);
-}
+	setTimeout(showTime, 1000);
+};
 
 // Add Zeros
 function addZero(n) {
-    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+	return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
-
-let images = [
-    "url('images/img-1.jpeg')",
-    "url('images/img-2.jpeg')",
-    "url('images/img-3.jpeg')",
-    "url('images/img-4.jpeg')",
-    "url('images/img-5.jpeg')"
-];
 
 // Set Background and Greeting
-function setBgGreet() {
-    let name = 'Master Josh';
+const setBgGreet = () => {
+	if (day === 16 && month === 2) {
+		greeting.textContent = 'Happy Birthday, ';
+		document.body.style.backgroundImage = "url('/img/bday.jpg')";
+	} else if (hour < 12) {
+		// Morning
+		document.body.style.backgroundImage = "url('/img/morning.jpg')";
+		greeting.textContent = 'Good Morning, ';
+	} else if (hour < 18) {
+		// Afternoon
+		document.body.style.backgroundImage = "url('/img/afternoon.jpg')";
+		greeting.textContent = 'Good Afternoon, ';
+	} else {
+		// Evening
+		document.body.style.backgroundImage = "url('/img/night.jpg')";
+		greeting.textContent = 'Good Evening, ';
+		document.body.style.color = 'white';
+	}
+};
 
-    let today = new Date(),
-        hour = today.getHours();
+async function fetchQuote() {
+	const response = await fetch('https://type.fit/api/quotes');
 
-    if (hour < 12) {
-        // Morning
-        document.body.style.backgroundImage =
-            "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-        greeting.textContent = 'Good Morning, Master Josh';
-    } else if (hour < 18) {
-        // Afternoon
-        document.body.style.backgroundImage =
-            "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-        greeting.textContent = 'Good Afternoon, Master Josh';
-    } else {
-        // Evening
-        document.body.style.backgroundImage =
-            "url('https://i.ibb.co/924T2Wv/night.jpg')";
-        greeting.textContent = 'Good Evening, Master Josh';
-        document.body.style.color = 'white';
-    }
+	if (!response.ok) {
+		const errorMessage = `There is a ${response.status} error.  I will fix it shortly. In the meantime, the offline quote list will be used. -Josh`;
+		offlineAPI();
+		throw new Error(errorMessage);
+	}
+
+	const data = await response.json();
+	return data;
 }
+fetchQuote().then((data) => {
+	const quoteElement = document.getElementById('quoteElement');
+	const authorElement = document.getElementById('authorElement');
 
-// Set Name
-function setName(e) {
-    if (e.type === 'keypress') {
-        // Make sure enter is pressed
-        if (e.which == 13 || e.keyCode == 13) {
-            localStorage.setItem('name', e.target.innerText);
-            name.blur();
-        }
-    } else {
-        localStorage.setItem('name', e.target.innerText);
-    }
-}
+	randomNum = Math.floor(Math.random() * data.length - 1);
+	quoteElement.textContent = data[randomNum].text;
+	authorElement.textContent = data[randomNum].author;
+	console.log(data[randomNum]);
+});
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-// focus.addEventListener('keypress', setFocus);
-// focus.addEventListener('blur', setFocus);
+const offlineAPI = () => {
+	randomNum = Math.floor(Math.random() * quoteList.length - 1);
+	quoteElement.textContent = quoteList[randomNum].text;
+	authorElement.textContent = quoteList[randomNum].author;
+	console.log(quoteList[randomNum]);
+};
 
 // Run
 showTime();
 setBgGreet();
-getName();
-// getFocus();
